1a38e15ec1e508db7d30f13d71175856b124ae08
Helio Camelo <106644445+heliocamelo@users.noreply.github.com>
2022-12-13 14:29:49 -0300

    Projeto completo de um gestor de estoque

---
+++ EBook.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestor_de_estoque
+{
+    [System.Serializable]
+    class EBook : Produto, IEstoque
+    {
+        public string autor;
+        private int vendas;
+
+        public EBook(string nome, float preco, string autor)
+        {
+            this.nome = nome;
+            this.preco = preco; 
+            this.autor = autor;
+        }
+
+        public void AdicionarEntrada()
+        {
+            Console.WriteLine("Não é possível dar entrada no estoque de um E-Book, pois é um produto digital");
+            Console.ReadLine();
+        }
+
+        public void AdicionarSaida()
+        {
+            Console.WriteLine($"Adicionar vendas no E-book {nome}");
+            Console.WriteLine("Digite a quantidade de vendas que você quer dar entrada: ");
+            int entrada = int.Parse(Console.ReadLine());
+            vendas = vendas + entrada;
+            Console.WriteLine("Saída registrada");
+            Console.ReadLine();
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Autor: {autor}");
+            Console.WriteLine($"Preço: {preco}");
+            Console.WriteLine($"Vendas: {vendas}");
+            Console.WriteLine("===============================");
+        }
+    }
+}
---
+++ ProdutoFisico.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestor_de_estoque
+{
+    [System.Serializable]
+    class ProdutoFisico : Produto, IEstoque
+    {
+        public float frete;
+        private int estoque;
+
+        public ProdutoFisico(string nome, float preco, float frete)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.frete = frete; 
+        }
+
+        public void AdicionarEntrada()
+        {
+            Console.WriteLine($"Adicionar entrada do produto {nome}");
+            Console.WriteLine("Digite a qunatidade que você quer dar entrada: ");
+            int entrada = int.Parse(Console.ReadLine());
+            estoque = estoque + entrada;
+            Console.WriteLine("Entrada registrada");
+            Console.ReadLine();
+        }
+
+        public void AdicionarSaida()
+        {
+            Console.WriteLine($"Adicionar saída no estoque do produto {nome}");
+            Console.WriteLine("Digite a quantidade que você quer dar baixa: ");
+            int entrada = int.Parse(Console.ReadLine());
+            estoque = estoque - entrada;
+            Console.WriteLine("Saída registrada");
+            Console.ReadLine();
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Frete: {frete}");
+            Console.WriteLine($"Preço: {preco}");
+            Console.WriteLine($"Quantidade em estoque: {estoque}");
+            Console.WriteLine("===============================");
+        }
+    }
+}
